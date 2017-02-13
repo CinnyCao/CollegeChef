b95148a87314d9cb8605be834df815a459fd0ad5
Cinny Cao <cinny.cao@caseware.com>
2017-02-13 16:58:59 -0500

    Fix rule of displaying menu items so that login/sign up will always show in larger screens

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,28 @@
 /* JS functions used to control navbar */
 $(function() {
+
     // hide menu items on start
-    $('.menu_item').hide();
+    $('.menu_item_left').hide();
+    // hide right menu items on start too in small screen
+    showHideRightMenuItems();
+    $(window).on('resize', function () {
+        showHideRightMenuItems();
+        showHideSiteName();
+    });
+
 });
 
+function showHideRightMenuItems() {
+    $('.menu_item_right').toggle($(window).width() > 600 || ($(window).width() <= 600 && $('.menu_item_left').is(':visible')));
+}
+
+function showHideSiteName() {
+    $('.site_name').toggle($(window).width() <= 600 || ($(window).width() > 600 && !$('.menu_item_left').is(':visible')));
+}
+
 function showLinks() {
-    $('.menu_item').toggle();
+    $('.menu_item_left').toggle();
+    showHideRightMenuItems();
     // Hide site name when showing menu items in large and medium screen size
-    if ($(window).width() > 600) {
-        $('.site_name').toggle(!$('.menu_item').is(':visible'));
-    }
+    showHideSiteName();
 }
