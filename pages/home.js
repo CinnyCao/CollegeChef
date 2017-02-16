8bd0251229da4fc0636f73d4992e8668eeda3103
Cinny Cao <cinny.cao@caseware.com>
2017-02-15 20:36:02 -0500

    Added search section to home page; make ingredient filter working

--- pages/home.js
+++ pages/home.js
@@ -2,3 +2,12 @@ $(function() {
     //load navbar
     $('#navbar_holder').load('../components/navbar.html');
 });
+
+function filterIngredients() {
+    var search_text = $('#ingredient_search_input').val().toLowerCase();
+    var ingredients = $(".ingredient_button");
+
+    for (i = 0; i < ingredients.length; i++) {
+        $(ingredients[i]).toggle(ingredients[i].title.toLowerCase().startsWith(search_text));
+    }
+}
