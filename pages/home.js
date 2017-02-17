85e2cceaeb258dd061febc14149a9644a3e6e9dc
Cinny Cao <cinny.cao@caseware.com>
2017-02-17 17:27:59 -0500

    Make ingredient buttons selectable and will be pinned when selected

--- pages/home.js
+++ pages/home.js
@@ -7,11 +7,17 @@ $(function () {
 
     // load recipe_card
     $('#hot_recipes').load('/components/recipe_card.html');
+
+    // pin and unpin ingredient buttons
+    $('.ingredient_button').on('click', function () {
+        $(this).toggleClass('selected_ingredient_button');
+        $(this).parent().prepend($('.selected_ingredient_button').after());
+    });
 });
 
 function filterIngredients() {
     var search_text = $('#ingredient_search_input').val().toLowerCase();
-    var ingredients = $(".ingredient_button");
+    var ingredients = $(".ingredient_button:not(.selected_ingredient_button)");
 
     for (i = 0; i < ingredients.length; i++) {
         $(ingredients[i]).toggle(ingredients[i].title.toLowerCase().startsWith(search_text));
