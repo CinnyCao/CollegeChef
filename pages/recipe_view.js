0baec416fe31ddf942da3d2cc96f394b1a3ebc52
Tanay Onder <tanayonder@gmail.com>
2017-02-16 20:35:13 -0500

    checking some weird error with image

---
+++ pages/recipe_view.js
@@ -0,0 +1,9 @@
+$(function() {
+    //load navbar
+    $('#navbar_holder').load('/components/navbar.html');
+    //load footer
+    $('#footer_holder').load('/components/footer.html');
+
+    // make ingredient search bar wider on medium screen
+});
+
