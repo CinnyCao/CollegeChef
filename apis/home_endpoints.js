58afaaf5cfee5165560fff3c92ee7eb656569560
Cinny Cao <cinny.cao@caseware.com>
2017-03-14 15:18:58 -0400

    finish get '/ingredients' endpoint

--- apis/home_endpoints.js
+++ apis/home_endpoints.js
@@ -2,13 +2,15 @@ module.exports = function (app, Recipe, Ingredient) {
     
     // get all ingredients
     app.get('/ingredients', function (req, res) {
-        Ingredient.find({}, function (err, allIngredients) {
+        Ingredient.find({}, '-_id id name imgUrl', function (err, allIngredients) {
             if (err) {
                 console.error(err);
-            } else {
-
             }
-
+            if (!allIngredients.length) {
+                return res.sendStatus(403);
+            } else {
+                res.json(allIngredients);
+            }
         });
     });
 };
