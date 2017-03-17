88e207d339dfc4047dfef6f5a19fef702223da93
zhoubecky <xiaoh.zhou@mail.utoronto.ca>
2017-03-16 22:09:39 -0400

    add auth check

--- apis/notification_endpoints.js
+++ apis/notification_endpoints.js
@@ -1,16 +1,25 @@
 module.exports = function (app, NotificationSetting, NotificationHistory) {
     // get user's notification settings
     app.get('/notification_settings', function (req, res) {
-        NotificationSetting.findOne({'personId': req.userID, 'enable': true}, 'typeNumber', function (err, notificationSettings) {
-            if (err) {
-                console.error(err);
-            }
-            if (!notificationSettings.length) {
-                return res.sendStatus(403);
-            } else {
-                res.json(notificationSettings);
-            }
-        });
+        if (req.auth)
+        {
+            NotificationSetting.findOne({'personId': req.userID, 'enable': true}, 'typeNumber', function (err, notificationSettings) {
+                if (err) {
+                    console.error(err);
+                }
+                if (!notificationSettings.length) {
+                    return res.sendStatus(403);
+                } else {
+                    res.json(notificationSettings);
+                }
+            });
+        }
+        else
+        {
+            return res.sendStatus(401);
+        }
     });
+
+
 
 };
