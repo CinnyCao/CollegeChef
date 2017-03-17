6f926b9aadb78c5d8ffb5e57ee935ae6259e86cc
Tanay Onder <tanayonder@gmail.com>
2017-03-17 17:07:19 -0400

    recipes_endpoints done, testing pending

--- apis/recipes_endpoints.js
+++ apis/recipes_endpoints.js
@@ -1,3 +1,21 @@
-module.exports = function (app, Recipe, Ingredient, sha1, generateToken, User) {
+module.exports = function (app, Recipe, Ingredient) {
+	// get all recipes
+	app.get('/recipes', function (req, res){
+		Recipe.find({}, '_id recipeName description imgUrl', function (err, allRecipes) {
+			if (err) {
+				console.error(err);
+			}
+			if (!allRecipes.length) {
+				return res.status(403).json({
+					status: 403,
+					message: "Get recipes failed: no ingredients found in database"
+				});
+				else {
+					res.json(allRecipes);
+				}
+			}
+		});
+
+	});
 
 };
