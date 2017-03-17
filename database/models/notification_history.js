01ee62216a183ad6115dc274772562d3044a1ff5
zhoubecky <xiaoh.zhou@mail.utoronto.ca>
2017-03-16 23:54:06 -0400

    change shcema of notificationSetting collection

--- database/models/notification_history.js
+++ database/models/notification_history.js
@@ -1,10 +1,13 @@
 module.exports = function (connection, Schema, autoIncrement) {
     var NotificationHistorySchema = new Schema({
         personId: {type: Number, required: true, ref: 'User'},
-        typeNumber: {type: Number, required: true, min: 1, max: 5},
+        // 0: uploaded recipe be rated; 1: uploaded recipe be commented
+        // 2: uploaded recipe be favorited; 3: uploaded recipe be modified
+        // 4: favorite recipe be modified
+        typeNumber: {type: Number, required: true, enum: [0, 1, 2, 3, 4]},
         createdDate: {type: Date, default: Date.now}
     });
-    
+
     NotificationHistorySchema.plugin(autoIncrement.plugin, 'NotificationHistory');
 
     return connection.model('NotificationHistory', NotificationHistorySchema);
