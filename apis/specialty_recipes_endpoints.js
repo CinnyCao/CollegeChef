07b2bcab6b7a728196f9705db9d3231b368a9ed0
Cinny <cinnabaria705@gmail.com>
2017-03-16 23:30:27 -0400

    finished GET /recipes/hot api

--- apis/specialty_recipes_endpoints.js
+++ apis/specialty_recipes_endpoints.js
@@ -3,8 +3,10 @@ module.exports = function (app, Recipe, IngredientToRecipe, Ingredient, Rate, Fa
     // search recipes by ingredients
     app.post('/search', function (req, res) {
         if (!req.body.ingredients) {
-            console.error("Search failed: Missing ingredients id list in request");
-            return res.sendStatus(400);
+            return res.status(400).json({
+                status: 400,
+                message: "Search failed: Missing ingredients id list in request"
+            });
         }
         var numOfIngredients = req.body.ingredients.length;
         IngredientToRecipe.aggregate(
@@ -35,22 +37,50 @@ module.exports = function (app, Recipe, IngredientToRecipe, Ingredient, Rate, Fa
                 // return only an array of result recipes with wanted fields
                 {"$project": {
                     "_id": 0,
-                    "ingredientCount": 1,
-                    "ingredients": 1,
+//                    "ingredientCount": 1,
+//                    "ingredients": 1,
                     "recipes._id": 1,
                     "recipes.recipeName": 1,
                     "recipes.description": 1,
                     "recipes.imgUrl": 1
-                }},
+                }}
             ], function (err, resultRecipes) {
                 res.json(resultRecipes);
             }
         )
     });
 
-    // get hot recipes
+    // get hot (mostly commented) recipes
     app.get("/recipes/hot", function (req, res) {
 
     });
+    
+    // get remarkable (highest rated) recipes
+    
+    // get new recipes
+    app.get("/recipes/new", function (req, res) {
+        Recipe.aggregate(
+            [
+                // sort by ModifiedDate
+                {"$sort": {"ModifiedDate": -1}},
+                // get only first 10
+                {"$limit" : 10 },
+                // set return fields
+                {"$project": {
+//                        "ModifiedDate": 1,
+                        "recipeName": 1,
+                        "description": 1,
+                        "imgUrl": 1
+                }}
+            ], function (err, resultRecipes) {
+                res.json(resultRecipes);
+            }
+        )
+    });
+    
+    // get favorited recipes
+    
+    // get recipes uploaded by current user
+    
 
 };
