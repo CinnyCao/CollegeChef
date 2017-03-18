d9e6c73d24f505656cc378b8336c98ca1fbddd8e
Tanay Onder <tanayonder@gmail.com>
2017-03-17 20:21:50 -0400

    recipe delete add/bug fix

--- apis/recipes_endpoints.js
+++ apis/recipes_endpoints.js
@@ -18,4 +18,39 @@ module.exports = function (app, Recipe, Ingredient) {
 
 	});
 
+	// delete a recipe
+    app.delete("/recipe/:recipeId", function (req, res) {
+        if (req.auth) {
+            // Admin can delete any recipe
+            if (req.isAdmin) {
+                Recipe.find({recipeId: parseInt(req.params.recipeId)}).remove().exec(function (err) {
+                    if (err) 
+                        return console.error(err);
+                    return res.sendStatus(200);
+                });
+            } 
+            else {
+               // check if the user trying to delete is the owner of the recipe
+               Recipe.find({recipeId: parseInt(req.params.recipeId)}, function (err, resultRecipe) {
+                    if (err)
+                        return console.error(err);
+                    if (req.userID == resultRecipe.personId) {
+                        resultRecipe.remove(function (err) {
+                            if (err)
+                                return console.error(err);
+                            return res.sendStatus(200);
+                        });
+                    }
+                });                
+            }
+                
+       } 
+        else { // user token expired
+            return res.status(401).json({
+                status: 401,
+                message: "Remove a recipe failed: unauthorized or token expired."
+            });
+       }
+    });
+
 };
