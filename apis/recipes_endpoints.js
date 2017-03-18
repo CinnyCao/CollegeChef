1f9314e4b34a9d996ee342d7954bf3f2d041818b
Cinny Cao <cinny.cao@caseware.com>
2017-03-17 21:17:25 -0400

    finish PUT "/recipe/:recipeId" api

--- apis/recipes_endpoints.js
+++ apis/recipes_endpoints.js
@@ -8,7 +8,7 @@ module.exports = function (app, Recipe, Ingredient) {
 			if (!allRecipes.length) {
                 return res.status(403).json({
                     status: 403,
-                    message: "Get recipes failed: no ingredients found in database"
+                    message: "Get recipes failed: no recipes found in database"
                 });
             }
 			else {
@@ -51,6 +51,57 @@ module.exports = function (app, Recipe, Ingredient) {
                 message: "Remove a recipe failed: unauthorized or token expired."
             });
        }
+    });
+
+    app.put("/recipe/:recipeId", function (req, res) {
+        if (req.auth) {
+            var toUpdate = {};
+            if (req.body.recipeName) {
+                toUpdate["recipeName"] = req.body.recipeName;
+            }
+            if (req.body.description) {
+                toUpdate["description"] = req.body.description;
+            }
+            if (req.body.instruction) {
+                toUpdate["instruction"] = req.body.instruction;
+            }
+            if (req.body.imgUrl) {
+                toUpdate["imgUrl"] = req.body.imgUrl;
+            }
+            if (req.body.numServings) {
+                toUpdate["numServings"] = req.body.numServings;
+            }
+            if (req.body.notes) {
+                toUpdate["notes"] = req.body.notes;
+            }
+            console.log(toUpdate.length);
+            if (Object.keys(toUpdate).length > 0) {
+                toUpdate["ModifiedById"] = req.userID;
+                toUpdate["ModifiedDate"] = Date.now();
+                Recipe.findOneAndUpdate({'_id': req.params.recipeId}, toUpdate, {new : true}, function (err, updatedRecipe) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    console.log(updatedRecipe);
+                    res.sendStatus(200);
+                });
+            } else {
+                return res.status(400).json({
+                    status: 400,
+                    message: "Update recipe failed: missing required input."
+                });
+            }
+        } else if (!req.auth) {
+            return res.status(401).json({
+                status: 401,
+                message: "Update Recipe failed: user deleted or token expired"
+            });
+        } else {
+            return res.status(401).json({
+                status: 401,
+                message: "Update Recipe failed: unauthorized"
+            });
+        }
     });
 
     app.get("/recipe/:recipeId", function (req, res) {
